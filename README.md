f600b15a2da17e4552907a6e32720c3b8477ee5c
santilapi13 <santilapiana02@gmail.com>
2025-06-30 14:31:55 -0300

    project and dependencies setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Notes App - Full Stack Implementation Excercise
+
---
+++ backend/src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
