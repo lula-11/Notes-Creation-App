ed0808eafa5f7791c86e48e3ee5a27d7ae9c3ff7
santilapi13 <santilapiana02@gmail.com>
2025-07-01 20:32:06 -0300

    feat: filter notes by category

--- backend/src/services/notes.service.js
+++ backend/src/services/notes.service.js
@@ -1,5 +1,6 @@
 import { Service } from './service.js';
 import { Note, Category } from '../models/models.js';
+import { categoriesService } from './categories.service.js';
 
 export class NotesService extends Service {
     constructor() {
@@ -11,6 +12,18 @@ export class NotesService extends Service {
         if (typeof query?.archived !== 'undefined') {
             if (query.archived === 'true') where.archived = true;
             else if (query.archived === 'false') where.archived = false;
+        }
+        if (typeof query?.category !== 'undefined') {
+            if (query.category === '-1') {
+                where.category = null;
+            } else {
+                const category = await categoriesService.getById(query.category);
+                if (!category) {
+                    return [];
+                }
+                
+                where.category = category.id;
+            }
         }
         return await this.model.findAll({ where, include: { model: Category } });
     }
