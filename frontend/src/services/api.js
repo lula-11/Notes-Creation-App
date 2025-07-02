079eae0a2414bb7d9dccc10ef4a7eaa396c156c3
santilapi13 <santilapiana02@gmail.com>
2025-07-01 22:47:12 -0300

    config: API endpoints communication in frontend

---
+++ frontend/src/services/api.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3000/api';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+api.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    console.error('API Error:', error);
+    throw error;
+  }
+);
+
+export const notesAPI = {
+  getAll: (params = {}) => api.get('/notes', { params }),
+  getById: (id) => api.get(`/notes/${id}`),
+  create: (data) => api.post('/notes', data),
+  update: (id, data) => api.patch(`/notes/${id}`, data),
+  delete: (id) => api.delete(`/notes/${id}`),
+};
+
+export const categoriesAPI = {
+  getAll: () => api.get('/categories'),
+  getById: (id) => api.get(`/categories/${id}`),
+  create: (data) => api.post('/categories', data),
+  update: (id, data) => api.patch(`/categories/${id}`, data),
+  delete: (id) => api.delete(`/categories/${id}`),
+};
+
+export default api; 
