015ac4f42832f024bb3ca3aee87c99809a6aaf16
santilapi13 <santilapiana02@gmail.com>
2025-07-02 16:24:41 -0300

    update: swagger documentation

--- backend/src/config/swagger.config.js
+++ backend/src/config/swagger.config.js
@@ -9,8 +9,8 @@ const swaggerOptions = {
         openapi: '3.0.1',
         info: {
             title: "Notes web app API",
-            description: "REST API for the management of notes."
-        }
+            description: `REST API for the management of notes.\n\n**How to test authentication:**\n1. Use the /api/sessions/login endpoint to get your accessToken and refreshToken.\n2. Click the 'Authorize' button and paste your accessToken.`,
+        },
     },
     apis: [`${__dirname}/../docs/**/*.yaml`]
 }
